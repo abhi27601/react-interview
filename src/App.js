a9c09f0b4df07d8a067b7e51e0fd9dc869e41dc5
abhi27601 <abhijitjoshi27601@gmail.com>
2021-03-31 17:35:18 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { store } from "../store/configureStore";
+import { store } from "./store/configureStore";
 import "./style.css";
 import AppRouter from "./routers/AppRouter";
 export default function App() {
